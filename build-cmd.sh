6af5960978c3a8cf8a0eee9d31ca45bfe3b7874a
Rye Mutt <rye@alchemyviewer.org>
2024-08-11 03:12:35 -0400

    Build script cleanup for all platforms including platform targeting and symbol generation

--- build-cmd.sh
+++ build-cmd.sh
@@ -29,15 +29,10 @@ source_environment_tempfile="$stage/source_environment.sh"
 "$autobuild" source_environment > "$source_environment_tempfile"
 . "$source_environment_tempfile"
 
+# remove_cxxstd
+source "$(dirname "$AUTOBUILD_VARIABLES_FILE")/functions"
+
 MESHOPT_SOURCE_DIR="meshoptimizer"
-
-# the name of the file to include in version.c
-VERSION_HEADER_FILE="$MESHOPT_SOURCE_DIR/src/meshoptimizer.h"
-
-# the name of the #define macro to print from the included header in version.c
-VERSION_MACRO="MESHOPTIMIZER_VERSION"
-
-build=${AUTOBUILD_BUILD_ID:=0}
 
 pushd "$MESHOPT_SOURCE_DIR"
     case "$AUTOBUILD_PLATFORM" in
@@ -45,11 +40,18 @@ pushd "$MESHOPT_SOURCE_DIR"
         windows*)
             load_vsvars
 
-            cmake ../${MESHOPT_SOURCE_DIR} -G "$AUTOBUILD_WIN_CMAKE_GEN" \
-                -A "$AUTOBUILD_WIN_VSPLATFORM" \
-                -DCMAKE_INSTALL_PREFIX="$(cygpath -m "$stage")"
-            build_sln "meshoptimizer.sln" "Release|$AUTOBUILD_WIN_VSPLATFORM" "Install"
+            opts="$(replace_switch /Zi /Z7 $LL_BUILD_RELEASE)"
+            plainopts="$(remove_switch /GR $(remove_cxxstd $opts))"
 
+            mkdir -p "build"
+            pushd "build"
+                cmake -G Ninja .. -DCMAKE_BUILD_TYPE=Release \
+                    -DCMAKE_C_FLAGS="$plainopts" \
+                    -DCMAKE_CXX_FLAGS="$opts" \
+                    -DCMAKE_INSTALL_PREFIX="$(cygpath -m "$stage")"
+                cmake --build . --config Release --parallel
+                cmake --install . --config Release
+            popd
 
             mkdir -p "$stage/lib/release"
             mv "$stage/lib/meshoptimizer.lib" \
@@ -60,61 +62,28 @@ pushd "$MESHOPT_SOURCE_DIR"
                 "$stage/include/meshoptimizer/meshoptimizer.h"
 
             rm -r "$stage/lib/cmake"
-
-            # populate version_file - prefer this method of regex extraction
-            # with a multitude of different tools - that can and does break over time.
-            cl /DVERSION_HEADER_FILE="\"$VERSION_HEADER_FILE\"" \
-               /DVERSION_MACRO="$VERSION_MACRO" \
-               /Fo"$(cygpath -w "$stage/version.obj")" \
-               /Fe"$(cygpath -w "$stage/version.exe")" \
-               "$(cygpath -w "$top/version.c")"
-            "$stage/version.exe" > "$stage/version.txt"
-            rm "$stage"/version.{obj,exe}
         ;;
 
         darwin*)
-            cmake . -DCMAKE_INSTALL_PREFIX:STRING="${stage}"
+            # Setup deploy target
+            export MACOSX_DEPLOYMENT_TARGET="$LL_BUILD_DARWIN_DEPLOY_TARGET"
 
-            make
-            make install
+            # Setup build flags
+            opts="${TARGET_OPTS:--arch $AUTOBUILD_CONFIGURE_ARCH $LL_BUILD_RELEASE}"
+            plainopts="$(remove_cxxstd $opts)"
 
-            mkdir -p "$stage/lib/release"
-            mv "$stage/lib/libmeshoptimizer.a" \
-                "$stage/lib/release/libmeshoptimizer.a"
+            mkdir -p "build"
+            pushd "build"
+                cmake -G Ninja .. \
+                    -DCMAKE_C_FLAGS="$opts" \
+                    -DCMAKE_CXX_FLAGS="$plainopts" \
+                    -DCMAKE_BUILD_TYPE=Release \
+                    -DCMAKE_INSTALL_PREFIX:STRING="${stage}" \
+                    -DCMAKE_OSX_ARCHITECTURES:STRING=x86_64 \
+                    -DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET}
 
-            mkdir -p "$stage/include/meshoptimizer"
-            mv "$stage/include/meshoptimizer.h" \
-                "$stage/include/meshoptimizer/meshoptimizer.h"
-
-            rm -r "$stage/lib/cmake"
-            
-            # populate version_file - prefer this method of regex extraction
-            # with a multitude of different tools - that can and does break over time.
-            cc -DVERSION_HEADER_FILE="\"$VERSION_HEADER_FILE\"" \
-               -DVERSION_MACRO="$VERSION_MACRO" \
-               -o "$stage/version" "$top/version.c"
-            "$stage/version" > "$stage/version.txt"
-            rm "$stage/version"
-        ;;
-
-        linux*)
-            opts="${TARGET_OPTS:--m$AUTOBUILD_ADDRSIZE $LL_BUILD_RELEASE}"
-
-            # Handle any deliberate platform targeting
-            if [ -z "${TARGET_CPPFLAGS:-}" ]; then
-                # Remove sysroot contamination from build environment
-                unset CPPFLAGS
-            else
-                # Incorporate special pre-processing flags
-                export CPPFLAGS="$TARGET_CPPFLAGS"
-            fi
-
-            rm -rf build && mkdir build
-            pushd build
-                cmake .. -DCMAKE_INSTALL_PREFIX:STRING="${stage}"
-    
-                make -j $AUTOBUILD_CPU_COUNT
-                make install
+                cmake --build . --config Release --parallel
+                cmake --install . --config Release
             popd
 
             mkdir -p "$stage/lib/release"
@@ -126,15 +95,41 @@ pushd "$MESHOPT_SOURCE_DIR"
                 "$stage/include/meshoptimizer/meshoptimizer.h"
 
             rm -r "$stage/lib/cmake"
+        ;;
 
-            # populate version_file - prefer this method of regex extraction
-            # with a multitude of different tools - that can and does break over time.
-            gcc -DVERSION_HEADER_FILE="\"$VERSION_HEADER_FILE\"" \
-               -DVERSION_MACRO="$VERSION_MACRO" \
-               -o "$stage/version" "$top/version.c"
-            "$stage/version" > "$stage/VERSION.txt"
-            rm "$stage/version"
+        linux*)
+            opts="${TARGET_OPTS:--m$AUTOBUILD_ADDRSIZE $LL_BUILD_RELEASE}"
+            plainopts="$(remove_cxxstd $opts)"
 
+            # Handle any deliberate platform targeting
+            if [ -z "${TARGET_CPPFLAGS:-}" ]; then
+                # Remove sysroot contamination from build environment
+                unset CPPFLAGS
+            else
+                # Incorporate special pre-processing flags
+                export CPPFLAGS="$TARGET_CPPFLAGS"
+            fi
+
+            mkdir -p "build"
+            pushd "build"
+                cmake -G Ninja .. \
+                    -DCMAKE_C_FLAGS="$opts" \
+                    -DCMAKE_CXX_FLAGS="$plainopts" \
+                    -DCMAKE_INSTALL_PREFIX:STRING="${stage}"
+    
+                cmake --build . --config Release --parallel
+                cmake --install . --config Release
+            popd
+
+            mkdir -p "$stage/lib/release"
+            mv "$stage/lib/libmeshoptimizer.a" \
+                "$stage/lib/release/libmeshoptimizer.a"
+
+            mkdir -p "$stage/include/meshoptimizer"
+            mv "$stage/include/meshoptimizer.h" \
+                "$stage/include/meshoptimizer/meshoptimizer.h"
+
+            rm -r "$stage/lib/cmake"
         ;;
     esac
     mkdir -p "$stage/LICENSES"
